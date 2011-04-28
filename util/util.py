6eafac5b20ba9b20c5a013880049101758bcce3d
Imanol Luengo <imaluengo@gmail.com>
2011-04-28 20:41:54 +0200

    * Removed migration core * Added Flex style -> CSS3 migration script

--- util/util.py
+++ util/util.py
@@ -9,42 +9,9 @@ def tabln(s, t, o):
    for line in lines:
        o.write(('\t'*t)+line+'\n')
 
-# Converts MXML attributes to CSS3
-def parseAttrs(attrs, outcss, chrome, firefox):
-    if "width" in attrs:
-        tabln("width: " + checkLen(attrs["width"]) + ";", 1, outcss)
-
-    if "minWidth" in attrs:
-        tabln("min-width: " + checkLen(attrs["minWidth"]) + ";", 1, outcss)
-
-    if "height" in attrs:
-        tabln("height: " + checkLen(attrs["height"]) + ";", 1, outcss)
-
-
+# 1000 -> 1000px by default
 def checkLen(s):
-    if s[-1].isdigit():
+    if s[-1].isdigit() and not(s[0] == "#"):
         return s+"px"
     else:
         return s
-
-
-def printHeader(out, cssfile):
-    out.write("""<!DOCTYPE html> 
-<html> 
-<head> 
-	<meta charset="UTF-8" /> 
-	<title>Output Title</title> 
- 
-	<!-- CSS --> 
-	<link rel="stylesheet" type="text/css" media="screen" href=\""""+cssfile+"""\" />
-</head>
-</body>
-<!-- Start of generated content -->
-
-""")
-
-def printFooter(out):
-    out.write("""
-<!-- End of generated content -->
-</body>
-</html>""")
